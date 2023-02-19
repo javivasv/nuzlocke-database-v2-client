8c620b76f2c29eb635f5cbde604dda7356a7667f
Javier Vivas <javivasv@gmail.com>
2023-02-18 21:24:23 -0500

    Vuetify and icons library setup

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,24 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { aliases, md } from "vuetify/iconsets/md";
 
-createApp(App).use(store).use(router).mount("#app");
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "md",
+    aliases,
+    sets: {
+      md,
+    },
+  },
+});
+
+createApp(App).use(store).use(router).use(vuetify).mount("#app");
