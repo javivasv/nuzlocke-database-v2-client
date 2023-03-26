420209047d7c7d7bcd3e05e773ebfcce8f918064
Javier Vivas <javivasv@gmail.com>
2023-03-26 16:30:53 -0400

    Home view remove bullet points from text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -56,28 +56,26 @@
           </v-card-subtitle>
           <v-row no-gutters>
             <v-card-text>
-              <ul>
-                <li>
-                  <p>
-                    Any Pokémon that faints is considered dead, and must be
-                    released or put in the Pokémon Storage System permanently
-                    (or may be transferred to another game, as long as the
-                    Pokémon is never able to be used again during this run)
-                  </p>
-                </li>
-                <li>
-                  <p>
-                    The player may only catch the first wild Pokémon encountered
-                    in each area, and none else. If the first wild Pokémon
-                    encountered faints or flees, there are no second chances. If
-                    the first encounter in the area is a double battle, the
-                    player is free to choose which of the two wild Pokémon they
-                    would like to catch but may only catch one of them. This
-                    restriction does not apply to Pokémon able to be captured
-                    during static encounters, nor to Shiny Pokémon
-                  </p>
-                </li>
-              </ul>
+              <v-row class="mb-5" no-gutters>
+                <span>
+                  Any Pokémon that faints is considered dead, and must be
+                  released or put in the Pokémon Storage System permanently (or
+                  may be transferred to another game, as long as the Pokémon is
+                  never able to be used again during this run).
+                </span>
+              </v-row>
+              <v-row no-gutters>
+                <span>
+                  The player may only catch the first wild Pokémon encountered
+                  in each area, and none else. If the first wild Pokémon
+                  encountered faints or flees, there are no second chances. If
+                  the first encounter in the area is a double battle, the player
+                  is free to choose which of the two wild Pokémon they would
+                  like to catch but may only catch one of them. This restriction
+                  does not apply to Pokémon able to be captured during static
+                  encounters, nor to Shiny Pokémon
+                </span>
+              </v-row>
             </v-card-text>
           </v-row>
         </v-card>
@@ -86,45 +84,43 @@
             <strong>Commonly accepted extra rules</strong>
           </v-card-subtitle>
           <v-card-text>
-            <ul>
-              <li>
-                <p>
-                  The two basic rules are not in effect until the player has
-                  gained their first Poké Balls and thus the ability to catch
-                  Pokémon
-                </p>
-              </li>
-              <li>
-                <p>
-                  The player must nickname all of their Pokémon, for the sake of
-                  forming stronger emotional bonds
-                </p>
-              </li>
-              <li>
-                <p>
-                  Species/Dupes Clause: The "first wild Pokémon in each area"
-                  rule does not apply in an area until a species or evolution
-                  line is encountered that has not been caught yet
-                </p>
-              </li>
-              <li>
-                <p>
-                  Shiny Clause: Shiny Pokémon do not need to be released if they
-                  faint
-                </p>
-              </li>
-              <li>
-                <p>
-                  If the player has no Pokémon that can use a field move that is
-                  required to continue the game, they may catch another Pokémon
-                  that can learn the required field move. However, this Pokémon
-                  cannot be used in battle for any reason, and must be released,
-                  permanently put into a PC box, or migrated as soon as it is no
-                  longer needed or if the player catches another Pokémon that
-                  can use this field move
-                </p>
-              </li>
-            </ul>
+            <v-row class="mb-5" no-gutters>
+              <span>
+                The two basic rules are not in effect until the player has
+                gained their first Poké Balls and thus the ability to catch
+                Pokémon
+              </span>
+            </v-row>
+            <v-row class="mb-5" no-gutters>
+              <span>
+                The player must nickname all of their Pokémon, for the sake of
+                forming stronger emotional bonds
+              </span>
+            </v-row>
+            <v-row class="mb-5" no-gutters>
+              <span>
+                Species/Dupes Clause: The "first wild Pokémon in each area" rule
+                does not apply in an area until a species or evolution line is
+                encountered that has not been caught yet
+              </span>
+            </v-row>
+            <v-row class="mb-5" no-gutters>
+              <span>
+                Shiny Clause: Shiny Pokémon do not need to be released if they
+                faint
+              </span>
+            </v-row>
+            <v-row no-gutters>
+              <span>
+                If the player has no Pokémon that can use a field move that is
+                required to continue the game, they may catch another Pokémon
+                that can learn the required field move. However, this Pokémon
+                cannot be used in battle for any reason, and must be released,
+                permanently put into a PC box, or migrated as soon as it is no
+                longer needed or if the player catches another Pokémon that can
+                use this field move
+              </span>
+            </v-row>
           </v-card-text>
         </v-card>
       </v-col>
