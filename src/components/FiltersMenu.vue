9f5436139c66e58d52778a15f6a6785acc60e948
Javier Vivas <javivasv@gmail.com>
2023-04-01 17:36:38 -0400

    Filters menu component reestructured

--- src/components/FiltersMenu.vue
+++ src/components/FiltersMenu.vue
@@ -1,64 +1,33 @@
 <template>
-  <v-card class="filters-card pa-4 mt-3 w-100" max-width="350px">
+  <v-card class="filters-card pa-4 mt-3" width="400px">
     <v-card-subtitle>
       <strong>Status filters</strong>
     </v-card-subtitle>
     <v-row no-gutters>
-      <v-checkbox
-        v-model="filters.alive"
-        label="Alive"
-        hide-details
-        color="secondary"
-        @update:modelValue="updateFilter('alive', $event)"
-      ></v-checkbox>
-      <v-checkbox
-        v-model="filters.fainted"
-        label="Fainted"
-        hide-details
-        color="secondary"
-        @update:modelValue="updateFilter('fainted', $event)"
-      ></v-checkbox>
+      <v-col v-for="filter in statusFilters" :key="filter.value" cols="4">
+        <v-checkbox
+          v-model="filter.on"
+          :label="filter.name"
+          hide-details
+          color="secondary"
+          @update:modelValue="updateFilter(filter.value, $event)"
+        ></v-checkbox>
+      </v-col>
     </v-row>
     <v-divider class="my-3"></v-divider>
     <v-card-subtitle>
       <strong>Obtained filters</strong>
     </v-card-subtitle>
     <v-row no-gutters>
-      <v-checkbox
-        v-model="filters.caught"
-        label="Caught"
-        hide-details
-        color="secondary"
-        @update:modelValue="updateFilter('caught', $event)"
-      ></v-checkbox>
-      <v-checkbox
-        v-model="filters.gifted"
-        label="Gifted"
-        hide-details
-        color="secondary"
-        @update:modelValue="updateFilter('gifted', $event)"
-      ></v-checkbox>
-      <v-checkbox
-        v-model="filters.hatched"
-        label="Hatched"
-        hide-details
-        color="secondary"
-        @update:modelValue="updateFilter('hatched', $event)"
-      ></v-checkbox>
-      <v-checkbox
-        v-model="filters.traded"
-        label="Traded"
-        hide-details
-        color="secondary"
-        @update:modelValue="updateFilter('traded', $event)"
-      ></v-checkbox>
-      <v-checkbox
-        v-model="filters.not"
-        label="Not caught"
-        hide-details
-        color="secondary"
-        @update:modelValue="updateFilter('not', $event)"
-      ></v-checkbox>
+      <v-col v-for="filter in obtainedFilters" :key="filter.value" cols="4">
+        <v-checkbox
+          v-model="filter.on"
+          :label="filter.name"
+          hide-details
+          color="secondary"
+          @update:modelValue="updateFilter(filter.value, $event)"
+        ></v-checkbox>
+      </v-col>
     </v-row>
   </v-card>
 </template>
@@ -72,15 +41,45 @@ export default defineComponent({
   computed: {},
   data() {
     return {
-      filters: {
-        alive: false,
-        fainted: false,
-        caught: false,
-        gifted: false,
-        hatched: false,
-        traded: false,
-        not: false,
-      },
+      statusFilters: [
+        {
+          name: "Alive",
+          value: "alive",
+          on: false,
+        },
+        {
+          name: "Fainted",
+          value: "fainted",
+          on: false,
+        },
+      ],
+      obtainedFilters: [
+        {
+          name: "Caught",
+          value: "caught",
+          on: false,
+        },
+        {
+          name: "Gifted",
+          value: "gifted",
+          on: false,
+        },
+        {
+          name: "Hatched",
+          value: "hatched",
+          on: false,
+        },
+        {
+          name: "Traded",
+          value: "traded",
+          on: false,
+        },
+        {
+          name: "Not caught",
+          value: "not",
+          on: false,
+        },
+      ],
     };
   },
   methods: {
